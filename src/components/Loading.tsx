d466373ff5b4374414ce2e9f6a9076f798c9044d
Ganeshdvp <ganeshcherupalli6565@gmail.com>
2026-03-18 23:46:09 +0530

    Fixed git comflicts

--- src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-export const Loading = ({color}) => {
-  return (
-    <>
-      <div
-        className={`w-4 h-4 ml-[50%] border-2 border-${color} border-t-transparent rounded-full animate-spin`}
-      />
-    </>
-  );
-};
