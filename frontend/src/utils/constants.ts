87493b831e8f3fe0f79d50f34d0b59dee487c666
Ganeshdvp <ganeshcherupalli6565@gmail.com>
2026-04-07 12:05:57 +0530

    Replace with correct urls

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL='http://localhost:3000'
+export const BASE_URL='https://fabricue.onrender.com'
 
 export const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/;
 
